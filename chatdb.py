cf7ab2837a4c177b8f4910802ba817c7e237749a
dst1213 <dst1213@126.com>
2023-07-14 16:27:20 +0800

    add semantic analysis

--- chatdb.py
+++ chatdb.py
@@ -105,7 +105,7 @@ def chain_of_memory(sql_steps, mysql_db):
     return sql_results_history, new_mem_ops
 
 
-def generate_chat_responses(user_inp, mysql_db, historical_message):
+def generate_chat_responses(user_inp, mysql_db, historical_message,use_semantic_answer=True):
     # ask steps
     prompt_ask_steps_str = prompt_ask_steps.format(user_inp=user_inp)
     response_steps = chat_with_ai(init_system_msg(), prompt_ask_steps_str, historical_message, None,
@@ -122,9 +122,10 @@ def generate_chat_responses(user_inp, mysql_db, historical_message):
     sql_results_history, new_mem_ops = chain_of_memory(response_steps_list_of_dict, mysql_db)
     # print(sql_results_history)
     # print(new_mem_ops)
-    result = semantic_handler(user_inp,response_steps_list_of_dict,sql_results_history)
-    color_text = get_colored_text(f"Answer:{result}",'green')
-    print(color_text)
+    if use_semantic_answer:
+        result = semantic_handler(user_inp,response_steps_list_of_dict,sql_results_history)
+        color_text = get_colored_text(f"Answer:{result}",'green')
+        print(color_text)
     print("Finish!")
     return
 
@@ -175,10 +176,11 @@ if __name__ == '__main__':
     # Whether to build examples using the sample files './csvs/*.csv'. Default is True. If data already exists,
     # such as in 'try1024.db', you can select False.
     init_db = False
+    use_semantic_answer = True
     mysql_db = init_database(database_info, "try1024", init_db=init_db)
     his_msgs = []
     print("START!")
     text = input("USER INPUT: ")
     while True:
-        generate_chat_responses(text, mysql_db, his_msgs)
+        generate_chat_responses(text, mysql_db, his_msgs,use_semantic_answer)
         text = input("USER INPUT: ")
